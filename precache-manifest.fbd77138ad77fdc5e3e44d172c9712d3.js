555eb76394a5eb4444e3d414c3d56265d994c2ad
Walter Wan <WalterHWan@gmail.com>
2020-02-23 21:15:14 -0800

    Updates

---
+++ precache-manifest.fbd77138ad77fdc5e3e44d172c9712d3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a88b8373693705c6b44c457eda55a285",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8b3fa7f6c6912aeeda61",
+    "url": "/static/css/main.a6b9ee40.chunk.css"
+  },
+  {
+    "revision": "1d4ed1b858ed68bbdbc7",
+    "url": "/static/js/2.7160cd2e.chunk.js"
+  },
+  {
+    "revision": "98061d712500f07672e3581cfdcd5a24",
+    "url": "/static/js/2.7160cd2e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b3fa7f6c6912aeeda61",
+    "url": "/static/js/main.534fe9e7.chunk.js"
+  },
+  {
+    "revision": "57bda6516de1d3bdee33",
+    "url": "/static/js/runtime-main.06618513.js"
+  }
+]);
