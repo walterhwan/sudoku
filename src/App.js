b33b075df9cc50e51854f2a67e74d0ea60a03f3e
Walter Wan <WalterHWan@gmail.com>
2020-02-23 22:15:32 -0800

    Add selected board

--- src/App.js
+++ src/App.js
@@ -1,39 +1,70 @@
-import React from "react";
-import PropTypes from 'prop-types';
-import { strToBoard } from "./helper";
-import "./App.css";
+import React from 'react'
+import PropTypes from 'prop-types'
+import { strToBoard } from './helper'
+import './App.css'
 
-const rawProblemStr = '004300209005009001070060043006002087190007400050083000600000105003508690042910300'
-const board = strToBoard(rawProblemStr)
+const rawProblemStr =
+  '004300209005009001070060043006002087190007400050083000600000105003508690042910300'
+const startBoard = strToBoard(rawProblemStr)
 
 // const board = (new Array(9).fill(0)).map(() => new Array(9).fill(0))
 
 const Cell = (props) => {
-  const { num = '0' } = props
+  const { num = '0', onClick, selected } = props
   const displayNum = num === '0' ? '' : num
-  // const displayNum = num
+  const id = selected ? 'selected' : ''
   return (
-    <div className="block"><p>{`${displayNum}`}</p></div>
+    <div className="cell" onClick={onClick} id={id}>
+      <p>{`${displayNum}`}</p>
+    </div>
   )
 }
 Cell.propTypes = {
   num: PropTypes.string,
+  onClick: PropTypes.func,
+  selected: PropTypes.bool,
+}
+
+function Board() {
+  const [board, setBoard] = React.useState(startBoard)
+  const [coord, setCoord] = React.useState([])
+
+  const handleOnClick = (rowIdx, colIdx) => () => {
+     // if click on the selected cell
+    if (rowIdx === coord[0] && colIdx === coord[1]) {
+      setCoord([])
+    } else {
+      setCoord([rowIdx, colIdx])
+    }
+  }
+
+  return (
+    <div className="board">
+      {board.map((row, rowIdx) => (
+        <div className="row" key={`row-${rowIdx}`}>
+          {row.map((cellNum, colIdx) => {
+            const selected = rowIdx === coord[0] && colIdx === coord[1]
+            return (
+              <Cell
+                selected={selected}
+                key={`cell-${rowIdx}-${colIdx}`}
+                num={cellNum}
+                onClick={handleOnClick(rowIdx, colIdx)}
+              />
+            )
+          })}
+        </div>
+      ))}
+    </div>
+  )
 }
 
 function App() {
   return (
     <div className="app">
-      <div className="board">
-        {board.map((row, rowIdx) => (
-          <div className="row" key={`row-${rowIdx}`}>
-            {row.map((cellNum, colIdx) => (
-              <Cell key={`cell-${rowIdx}-${colIdx}`} num={cellNum}/>
-            ))}
-          </div>
-        ))}
-      </div>
+      <Board/>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
